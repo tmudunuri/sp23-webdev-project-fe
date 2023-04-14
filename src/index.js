29e27363ba1cfc40e263af9636d745aadae21e5d
Thrivikram Mudunuri <23713701+tmudunuri@users.noreply.github.com>
2023-04-13 22:44:13 -0400

    Init

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { createRoot } from 'react-dom/client';
+import App from "./App";
+import Modal from "react-modal";
+import { UserProvider } from "context/UserContext";
+
+Modal.setAppElement("#root");
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
+    <UserProvider>
+        <App />
+    </UserProvider>)
+    ;
